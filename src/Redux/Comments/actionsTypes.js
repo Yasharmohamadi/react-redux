62e6fb9b72ea1abe02e3ae324f4d036e38786907
Yashar <yashar22mo@gmail.com>
2024-11-04 20:48:23 +0330

    add ducks pattern for Comments store

--- src/Redux/Comments/actionsTypes.js
+++
@@ -1,2 +0,0 @@
-export const addCommentActionType = 'ADD_COMMENT'
-export const removeCommentActionType = 'REMOVE_COMMENT'
--- src/Redux/Stores/Comments.js
+++ src/Redux/Stores/Comments.js
@@ -1,3 +1,43 @@
 // Actions
+const addCommentActionType = "ADD_COMMENT";
+const removeCommentActionType = "REMOVE_COMMENT";
+
 // Reducer
+export default function Reducer(state = [], action) {
+	switch (action.type) {
+		case addCommentActionType: {
+			let newComment = {
+				id: action.id,
+				tilte: action.title,
+				body: action.body,
+			};
+			return [...state, newComment];
+		}
+
+		case removeCommentActionType: {
+			let filteredState = [...state].filter(
+				(course) => course.id !== action.id
+			);
+			return filteredState;
+		}
+		default: {
+			return state;
+		}
+	}
+}
 // Actions Creators
+export const addCommentActionCreator = (id, title, body) => {
+	return {
+		id,
+		title,
+		body,
+		type: addCommentActionType,
+	};
+};
+
+export const removeCommentActionCreator = (id) => {
+	return {
+		id,
+		type: removeCommentActionType,
+	};
+};
