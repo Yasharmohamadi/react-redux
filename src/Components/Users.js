f6e26ba56d336723c3781ee1d524fc159367bbfe
Yashar <yashar22mo@gmail.com>
2024-11-07 11:26:15 +0330

    add useDispatch hook to User component

--- src/Components/Users.js
+++ src/Components/Users.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addUserActionCreator } from "../Redux/Stores/Users";
 
-function Users(props) {
+export default function Users() {
+	const state = useSelector()
+	const dispatch = useDispatch();
+
+	const addNewUser = () => {
+		dispatch(addUserActionCreator(5, "Eshi"));
+	};
 	return (
 		<div className="text-[#5f9ea0] p-4">
 			<h1 className="text-xl">Users</h1>
-			<button className="btn text-black text-sm" onClick={props.addNewUser}>
+			<button className="btn text-black text-sm" onClick={addNewUser}>
 				Add New User
 			</button>
 			<ul className="list-disc pl-4">
@@ -23,11 +29,3 @@ function Users(props) {
 function mapStateToProps(state) {
 	return { users: state.Users };
 }
-
-function mapDispatchToProps(dispatch) {
-	return {
-		addNewUser: () => dispatch(addUserActionCreator(5, "Eshi")),
-	};
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
